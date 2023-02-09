8a01900ebb74a66b8ad246529661ab8d6cd7103a
rashodk <rmanamperiko@mun.ca>
2023-02-09 20:08:35 -0330

    added photos for photography page

---
+++ src/components/gallery/gallery.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+type Props = {};
+
+const Gallery = (props: Props) => {
+  return (
+    <div className="bg-bg w-full">
+      <div className="container mx-auto">
+        <div className="flex flex-col items-center justify-center">
+          <h1 className="text-4xl font-bold text-center text-primary">Gallery</h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Gallery;
