d768944e0ec59abed0b670b0446ce82632ec76e5
rashodk <rmanamperiko@mun.ca>
2023-02-09 23:05:05 -0330

    updated link

--- src/components/main/work.tsx
+++ src/components/main/work.tsx
@@ -30,7 +30,7 @@ const work = (props: Props) => {
               Simple website for a local cleaning company. Built Using Html and pure CSS.
             </p>
             <div className="flex justify-center">
-              <Link href="https://squeekleen.com/" target="_blank" rel="noreferrer">
+              <Link href="https://dev.squeekleen.com/" target="_blank" rel="noreferrer">
                 <button className="bg-black text-white px-4 py-2 rounded-3xl ring-2 ring-blue-500 hover:scale-110 transition-all duration-1000 ease-in-out">
                   View Project
                 </button>
