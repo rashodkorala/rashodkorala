13015da18e2310e524eecb0b3cbaa59c0c29695e
rashodk <rmanamperiko@mun.ca>
2023-11-26 17:59:54 -0330

    change the email button

--- src/components/main/contact.tsx
+++ src/components/main/contact.tsx
@@ -8,8 +8,8 @@ const Contact = (props: Props) => {
       <div className="max-w-[1000px] mx-auto px-4 flex flex-col justify-center items-center h-full text-white xsm:px-5">
         <h1 className="text-4xl font-bold">Let&apos;s Connect</h1>
         <div>
-          <p className="text-md my-4">Feel free to reach out via email:</p>
-          <a href="mailto:your.email@example.com" className="bg-cyan-600 border-2 border-black rounded-[40px] px-8 py-2 text-white">
+          <p className="text-md my-4 font-sans font-semibold">Feel free to reach out via email</p>
+          <a href="mailto:your.email@example.com" className="flex justify-center bg-black text-white px-2 py-2 rounded-3xl ring-2 ring-blue-500 my-4 hover:scale-110 transition-all duration-1000 ease-in-out ">
             Email
           </a>
         </div>
